0dc7ab8b06ac05e764877d22de2358ea9a70f0a6
wrehas <wrehas@gmail.com>
2017-04-30 13:09:25 +0300

    add location coordinates

--- src/app/kitty/kitty.component.ts
+++ src/app/kitty/kitty.component.ts
@@ -14,17 +14,17 @@ import { Constants } from "../constants";
 export class KittyComponent implements OnInit {
 
   public flight : any;
-  public locations : any[];
 
   constructor(private kittyService:KittyService) { }
 
   ngOnInit() {
-    this.locations = Constants.locations;
+    var locations = Constants.locations;
     this.kittyService.getFlights()
       .subscribe(
         subscr => {
           this.flight = subscr;
-          console.log(subscr);
+          this.flight.location = locations.find(location => location.Id == this.flight.place.code);
+          console.log(this.flight);
         },
         err => console.log(err));
   }
